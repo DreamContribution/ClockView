06a4b25b9f3e61264624e69fb11b5af044a929b0
DreamContribution <Duke@Duke.local>
2015-11-08 20:06:50 +0800

    适应了新View的Activity

--- app/src/main/java/com/duke/clockview/MainActivity.java
+++ app/src/main/java/com/duke/clockview/MainActivity.java
@@ -5,12 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ClockView cv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-        cv= (ClockView) findViewById(R.id.clock_view);
-        cv.getStart();
     }
 }
